54e74116774266319d665977afc159005679aaa6
Ariel Moskovich <ariel@appsflyer.com>
2018-01-17 15:58:07 +0200

    change install.sh

--- session-3/nomad/install.sh
+++ session-3/nomad/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo mkdir -p /opt/nomad/bin /opt/nomad/conf /opt/nomad/data; /opt/nomad/logs /opt/consul/data /opt/consul/bin, /opt/consul/logs; sudo chown -R ubuntu:ubuntu /opt/nomad /opt/consul
+sudo mkdir -p /opt/nomad/bin /opt/nomad/conf /opt/nomad/data; /opt/nomad/logs /opt/consul/data /opt/consul/bin /opt/consul/logs; sudo chown -R ubuntu:ubuntu /opt/nomad /opt/consul
 sudo apt-get install git unzip jq -y
 sudo wget https://releases.hashicorp.com/nomad/0.7.1/nomad_0.7.1_linux_amd64.zip -O nomad.zip;sudo unzip nomad.zip -d /opt/nomad/bin
 sudo wget https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_amd64.zip -O consul.zip;sudo  unzip consul.zip -d /opt/consul/bin
